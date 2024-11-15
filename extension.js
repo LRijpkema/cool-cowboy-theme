2d8e15b69176310cea382399baf04cfda680793d
Lulu Rijpkema <lulurijpkema@gmail.com>
2024-11-15 16:03:19 +0100

    Fixed icon extension

--- extension.js
+++ extension.js
@@ -1,7 +1,15 @@
 const vscode = require('vscode');
 
 function activate(context) {
-    vscode.workspace.getConfiguration().update('workbench.iconTheme', 'vscode-jetbrains-icon-theme-2023-dark', true);
+    // Set icon theme
+    let disposable = vscode.commands.registerCommand('cool-cowboy-theme.activate', () => {
+        vscode.workspace.getConfiguration().update('workbench.iconTheme', 'vscode-jetbrains-icon-theme-2023-dark', true);
+    });
+
+    context.subscriptions.push(disposable);
+
+    // Run activation command
+    vscode.commands.executeCommand('cool-cowboy-theme.activate');
 }
 
-exports.activate = activate;
+module.exports = { activate };
