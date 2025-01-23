745b65b84adf9209862fb09fd9da30c7d58d433e
bruno-c-p <bruno.cardozo.pereira@gmail.com>
2025-01-23 17:39:05 -0300

    feat: remove automatic icon theme configuration

--- extension.js
+++ extension.js
@@ -1,15 +1,7 @@
 const vscode = require('vscode');
 
 function activate(context) {
-    // Set icon theme
-    let disposable = vscode.commands.registerCommand('cool-cowboy-theme.activate', () => {
-        vscode.workspace.getConfiguration().update('workbench.iconTheme', 'vscode-jetbrains-icon-theme-2023-dark', true);
-    });
-
-    context.subscriptions.push(disposable);
-
-    // Run activation command
-    vscode.commands.executeCommand('cool-cowboy-theme.activate');
+    console.log('Cool Cowboy Theme extension is now active!');
 }
 
 module.exports = { activate };
